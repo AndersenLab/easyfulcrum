700531f07ab1c241f2434d6cc7b5b32598a8dc7d
Tim Crombie <tim.crombie@gmail.com>
2020-08-11 21:22:53 -0500

    use exported data rather than .csv

--- R/annotateFulcrum.R
+++ R/annotateFulcrum.R
@@ -13,7 +13,7 @@ annotateFulcrum <- function(data) {
   # assign data to joined_data
   joined_data <- data
   # import island csv, this will have to change once we implement CI
-  island <- readr::read_csv("test_data/island.csv")
+  island <- island
 
   # Create Island Column
   joined_data$collection_island <- NA_character_
@@ -24,7 +24,7 @@ annotateFulcrum <- function(data) {
   }
 
   # import location csv, this will have to change once we implement CI
-  location <- readr::read_csv("test_data/location.csv")
+  location <- location
 
   # Create location Column
   joined_data$collection_location <- NA_character_
@@ -37,7 +37,7 @@ annotateFulcrum <- function(data) {
   # These polygons are manually curated by using the polygon tool.
 
   # import trails csv, this will have to change once we implement CI
-  trails_df <- readr::read_csv("test_data/trails.csv")
+  trails_df <- trails
 
   trails <- as.list(trails_df$coordinates)
   names(trails) <- trails_df$trail_name
