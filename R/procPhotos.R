6f078704f1ffc5b8c7019a7bf4b0ee1d43391e3c
Tim Crombie <tim.crombie@gmail.com>
2020-08-12 22:18:57 -0500

    remove thumbnail suffix

--- R/procPhotos.R
+++ R/procPhotos.R
@@ -1,13 +1,13 @@
 #' procPhotos
 #'
-#' \code{procPhotos} copies raw sample photos from the Fulcrum export and renames them to strain_name_C-label for use with CeNDR. The function also makes thumbnails.
+#' \code{procPhotos} copies raw sample photos from the Fulcrum export and renames them to strain_name for use with CeNDR. The function also resizes images.
 #'
 #' @param dir a directory with sample photos.
 #' @param data a data frame output from the \code{joinGenoFulc} function.
-#' @param max_dim This value sets the maximum dimension of the thumbnail images in pixels. The default value is 500.
+#' @param max_dim This value sets the maximum dimension of the resized images in pixels. The default value is 500.
 #' @param overwrite Logical, passed to fs::file_copy. If \code{TRUE} then existing files with similar names will be written over. Default is \code{FALSE}.
 #' @return A folder named processed_photos in the data/processed/fulcrum directory. The folder contains full size sample photos renamed with strain names.
-#' A thumbnails subfolder is also returned within the processed_photos folder. This folder contains image thumbnails. A dataframe identical to input \code{data}
+#' A thumbnails subfolder is also returned within the processed_photos folder. This folder contains resized images. A dataframe identical to input \code{data}
 #' with md5 hash values and file names for all photos.
 #' @importFrom rebus ALPHA one_or_more %R% DGT WRD optional
 #' @importFrom imager load.image resize save.image
@@ -23,13 +23,13 @@ procPhotos <- function(dir, data, max_dim = 500, overwrite = FALSE) {
     dplyr::filter(!is.na(ECA_name)) %>%
     dplyr::mutate(orig_file_name = glue::glue("{dir}/{sample_photo1}.jpg"),
                   new_file_name = glue::glue("{processed_dir}/processed_photos/{ECA_name}.jpg"),
-                  thumb_file_name = glue::glue("{processed_dir}/processed_photos/thumbnails/{ECA_name}_thumbnail.jpg")) %>%
+                  thumb_file_name = glue::glue("{processed_dir}/processed_photos/thumbnails/{ECA_name}.jpg")) %>%
     dplyr::mutate(orig_file_name2 = glue::glue("{dir}/{sample_photo2}.jpg"),
                   new_file_name2 = glue::glue("{processed_dir}/processed_photos/{ECA_name}_2.jpg"),
-                  thumb_file_name2 = glue::glue("{processed_dir}/processed_photos/thumbnails/{ECA_name}_2_thumbnail.jpg")) %>%
+                  thumb_file_name2 = glue::glue("{processed_dir}/processed_photos/thumbnails/{ECA_name}_2.jpg")) %>%
     dplyr::mutate(orig_file_name3 = glue::glue("{dir}/{sample_photo3}.jpg"),
                   new_file_name3 = glue::glue("{processed_dir}/processed_photos/{ECA_name}_3.jpg"),
-                  thumb_file_name3 = glue::glue("{processed_dir}/processed_photos/thumbnails/{ECA_name}_3_thumbnail.jpg")) %>%
+                  thumb_file_name3 = glue::glue("{processed_dir}/processed_photos/thumbnails/{ECA_name}_3.jpg")) %>%
     dplyr::select(ECA_name, species_id, c_label, sample_photo1, sample_photo2, sample_photo3, orig_file_name:thumb_file_name3)
 
   to_change1 <- to_change %>%
@@ -120,12 +120,12 @@ procPhotos <- function(dir, data, max_dim = 500, overwrite = FALSE) {
   raw_hash3 <- raw_hash %>%
     dplyr::filter(sample_photo %in% data$sample_photo3)
 
-  message("Writing md5 hash for thumbnails and adding to data frame.")
+  message("Writing md5 hash for resized images and adding to data frame.")
   thumb_hash <- dplyr::as_tibble(as.list(tools::md5sum(fs::dir_ls(glue::glue("{processed_dir}/processed_photos/thumbnails"))))) %>%
-    tidyr::gather(key = thumb_file, value = sample_photo_thumbnail_hash) %>%
-    dplyr::mutate(sample_photo_thumbnail_file_name = stringr::str_extract(thumb_file, pattern = one_or_more(WRD) %R% optional("_") %R% optional(DGT) %R% "_thumbnail.jpg"),
-                  ECA_name = stringr::str_extract(sample_photo_thumbnail_file_name, pattern = ALPHA %R% ALPHA %R% optional(ALPHA) %R% optional(ALPHA) %R% DGT %R% DGT %R% DGT %R% DGT %R% optional(DGT)),
-                  sample_photo_processed_file_name =stringr::str_replace(sample_photo_thumbnail_file_name, pattern = "_thumbnail.jpg", replacement = ".jpg"))
+    tidyr::gather(key = thumb_file, value = sample_photo_resized_hash) %>%
+    dplyr::mutate(sample_photo_resized_file_name = stringr::str_extract(thumb_file, pattern = one_or_more(WRD) %R% optional("_") %R% optional(DGT) %R% ".jpg"),
+                  ECA_name = stringr::str_extract(sample_photo_resized_file_name, pattern = ALPHA %R% ALPHA %R% optional(ALPHA) %R% optional(ALPHA) %R% DGT %R% optional(DGT) %R% optional(DGT) %R% optional(DGT) %R% optional(DGT)),
+                  sample_photo_processed_file_name = sample_photo_resized_file_name)
 
   # get hash 1
   thumb_hash1 <- thumb_hash %>%
@@ -145,18 +145,18 @@ procPhotos <- function(dir, data, max_dim = 500, overwrite = FALSE) {
                                    sample_photo2_raw_file_name = sample_photo_raw_file_name, sample_photo), by = c("sample_photo2" = "sample_photo")) %>%
     dplyr::left_join(dplyr::select(raw_hash3, sample_photo3_hash = sample_photo_raw_photo_hash,
                                    sample_photo3_raw_file_name = sample_photo_raw_file_name, sample_photo), by = c("sample_photo3" = "sample_photo")) %>%
-    dplyr::left_join(dplyr::select(thumb_hash1, sample_photo1_thumbnail_hash = sample_photo_thumbnail_hash,
+    dplyr::left_join(dplyr::select(thumb_hash1, sample_photo1_resized_hash = sample_photo_resized_hash,
                                    sample_photo1_processed_file_name = sample_photo_processed_file_name,
-                                   sample_photo1_thumbnail_file_name = sample_photo_thumbnail_file_name, ECA_name), by = c("ECA_name" = "ECA_name")) %>%
-    dplyr::left_join(dplyr::select(thumb_hash2, sample_photo2_thumbnail_hash = sample_photo_thumbnail_hash,
+                                   sample_photo1_resized_file_name = sample_photo_resized_file_name, ECA_name), by = c("ECA_name" = "ECA_name")) %>%
+    dplyr::left_join(dplyr::select(thumb_hash2, sample_photo2_resized_hash = sample_photo_resized_hash,
                                    sample_photo2_processed_file_name = sample_photo_processed_file_name,
-                                   sample_photo2_thumbnail_file_name = sample_photo_thumbnail_file_name, ECA_name), by = c("ECA_name" = "ECA_name")) %>%
-    dplyr::left_join(dplyr::select(thumb_hash3, sample_photo3_thumbnail_hash = sample_photo_thumbnail_hash,
+                                   sample_photo2_resized_file_name = sample_photo_resized_file_name, ECA_name), by = c("ECA_name" = "ECA_name")) %>%
+    dplyr::left_join(dplyr::select(thumb_hash3, sample_photo3_resized_hash = sample_photo_resized_hash,
                                    sample_photo3_processed_file_name = sample_photo_processed_file_name,
-                                   sample_photo3_thumbnail_file_name = sample_photo_thumbnail_file_name, ECA_name), by = c("ECA_name" = "ECA_name")) %>%
-    dplyr::select(project:sample_photo1, sample_photo1_raw_file_name, sample_photo1_processed_file_name, sample_photo1_hash, sample_photo1_thumbnail_file_name, sample_photo1_thumbnail_hash,
-                  sample_photo2, sample_photo2_raw_file_name, sample_photo2_processed_file_name, sample_photo2_hash, sample_photo2_thumbnail_file_name, sample_photo2_thumbnail_hash,
-                  sample_photo3, sample_photo3_raw_file_name, sample_photo3_processed_file_name, sample_photo3_hash, sample_photo3_thumbnail_file_name, sample_photo3_thumbnail_hash,
+                                   sample_photo3_resized_file_name = sample_photo_resized_file_name, ECA_name), by = c("ECA_name" = "ECA_name")) %>%
+    dplyr::select(project:sample_photo1, sample_photo1_raw_file_name, sample_photo1_processed_file_name, sample_photo1_hash, sample_photo1_resized_file_name, sample_photo1_resized_hash,
+                  sample_photo2, sample_photo2_raw_file_name, sample_photo2_processed_file_name, sample_photo2_hash, sample_photo2_resized_file_name, sample_photo2_resized_hash,
+                  sample_photo3, sample_photo3_raw_file_name, sample_photo3_processed_file_name, sample_photo3_hash, sample_photo3_resized_file_name, sample_photo3_resized_hash,
                   everything())
   # return
   message("DONE")
