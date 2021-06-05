91286aee63990d27b4b8381df9dcff0ac6a79a6a
Tim Crombie <tim.crombie@gmail.com>
2021-06-05 16:01:29 -0500

    add raw files, edit vignette

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -42,3 +42,40 @@ makeDirStructure <- function(startdir, projectdirname) {
   dir.create(file.path(startdir, projectdirname, "reports"), recursive = TRUE)
   dir.create(file.path(startdir, projectdirname, "scripts"), recursive = TRUE)
 }
+
+#' loadExampleFiles
+#'
+#' \code{loadExampleFiles} Copies raw .csv and .jpg files from package into example directory.
+#'
+#' @param startdir the working folder where project files will lie.
+#' @param projectdirname the name of the project, folder will be constructed under \code{startdir}.
+#' @import stringr
+#' @import fs
+#' @export
+#'
+
+loadExampleFiles <- function(startdir, projectdirname) {
+  # Find extdata folder path
+  files_path <- stringr::str_replace(system.file("extdata", "nematode_isolation.csv", package = "easyfulcrum", mustWork = TRUE),
+                                     pattern = "nematode_isolation.csv", replacement = "")
+
+  # Make vecotr of .csv and .jpg file paths
+  csv <- stringr::str_subset(list.files(files_path), pattern = ".csv")
+  csv_paths <- paste0(files_path, csv)
+  jpg <- stringr::str_subset(list.files(files_path), pattern = ".jpg")
+  jpg_paths <- paste0(files_path, jpg)
+
+  # make path to example project directory
+  csv_example_dir <- paste0(startdir,"/", projectdirname, "/data/raw/fulcrum/")
+  jpg_example_dir <- paste0(startdir,"/", projectdirname, "/data/raw/fulcrum/photos/")
+  csv_new_paths <- paste0(csv_example_dir, csv)
+  jpg_new_paths <- paste0(jpg_example_dir, jpg)
+
+  # make df to pass to file_copy
+  c_copy <- tibble::tibble(csv_paths, csv_new_paths)
+  j_copy <- tibble::tibble(jpg_paths, jpg_new_paths)
+
+  # copy files in package to example project folder
+  fs::file_copy(c_copy$csv_paths, c_copy$csv_new_paths, overwrite = F)
+  fs::file_copy(j_copy$jpg_paths, j_copy$jpg_new_paths, overwrite = F)
+}
